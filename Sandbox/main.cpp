15a43a666b4a3a4aa34919824a77eaac660acad4
SpinnerX <heraaronhotmail@yahoo.com>
2024-02-23 15:51:12 -0800

    Added testing sample code

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,9 +1,13 @@
 #include <Engine3D/Core/Application.h>
+#include <Engine3D/Core/EngineLogger.h>
 #include <Engine3D/Core/EntryPoint.h>
+
 
 class ExampleApp : public Engine3D::Application{
 public:
-	ExampleApp() : Application(){}
+	ExampleApp() : Application(){
+		coreLogInfo("This is an interesting logger lol\n");
+	}
 };
 
 Engine3D::Application* Engine3D::CreateApplication(){
