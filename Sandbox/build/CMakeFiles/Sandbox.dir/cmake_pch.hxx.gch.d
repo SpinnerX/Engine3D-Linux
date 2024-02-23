624ac53423f53bd37036fed26c394f6707863db4
SpinnerX <heraaronhotmail@yahoo.com>
2024-02-23 15:54:17 -0800

    Deleting build dir from repo

--- Sandbox/build/CMakeFiles/Sandbox.dir/cmake_pch.hxx.gch.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Sandbox.dir/cmake_pch.hxx.gch: \
- /home/spinnerx/Desktop/Engine3D-Linux/Sandbox/build/CMakeFiles/Sandbox.dir/cmake_pch.hxx.cxx \
- /usr/include/stdc-predef.h \
- /home/spinnerx/Desktop/Engine3D-Linux/Sandbox/build/CMakeFiles/Sandbox.dir/cmake_pch.hxx \
- /home/spinnerx/Desktop/Engine3D-Linux/include/Engine3D/Engine3DPrecompiledHeader.h
--- Sandbox/build/CMakeFiles/Sandbox.dir/main.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/Sandbox.dir/main.cpp.o: \
- /home/spinnerx/Desktop/Engine3D-Linux/Sandbox/main.cpp \
- /usr/include/stdc-predef.h \
- /usr/local/include/Engine3D/Core/Application.h \
- /usr/local/include/Engine3D/Core/core.h \
- /usr/local/include/Engine3D/Core/EntryPoint.h
