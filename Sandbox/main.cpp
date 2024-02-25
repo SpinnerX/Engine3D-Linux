58bd962ad7eb47516d46ec80276d6b36699682f1
SpinnerX <heraaronhotmail@yahoo.com>
2024-02-24 17:47:16 -0800

    Updated Sandbox

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,12 +1,24 @@
 #include <Engine3D/Core/Application.h>
 #include <Engine3D/Core/EngineLogger.h>
 #include <Engine3D/Core/EntryPoint.h>
+#include <Engine3D/Core/LayerStack.h>
 
+class ExampleLayer : public Engine3D::Layer{
+public:
+	void onUpdate() override {
+        clientLogInfo("ExampleLayer::Update");
+    }
+
+    void onEvent(Engine3D::Event& event) override{
+        clientLogTrace("{}", event);
+    }
+};
 
 class ExampleApp : public Engine3D::Application{
 public:
 	ExampleApp() : Application(){
-		coreLogInfo("This is an interesting logger lol\n");
+		pushLayer(new ExampleLayer());
+		/* coreLogInfo("This is an interesting logger lol\n"); */
 	}
 };
 
