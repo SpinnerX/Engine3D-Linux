3e657a412decfc638348ffc8751fde39eccebf10
= <=>
2024-03-11 15:39:19 -0700

    Updated and refactored file structures

---
+++ Debug/src/CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o.d
@@ -0,0 +1,3 @@
+src/CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o: \
+  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/share/asan_ignorelist.txt \
+  /Users/aaronher/Desktop/Engine3D-Linux/src/Engine3D/Core/core.cpp
---
+++ Debug/src/CMakeFiles/Engine3D.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/Application.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/Application.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/EngineLogger.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/EngineLogger.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/EntryPoint.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/EntryPoint.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/Layer.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/Layer.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/LayerStack.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/LayerStack.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/Events/InputPoll.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/Events/InputPoll.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/platforms/Linux/LinuxInputPoll.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/platforms/Linux/LinuxInputPoll.cpp.o.d"
+  "CMakeFiles/Engine3D.dir/Engine3D/platforms/Linux/LinuxWindow.cpp.o"
+  "CMakeFiles/Engine3D.dir/Engine3D/platforms/Linux/LinuxWindow.cpp.o.d"
+  "libEngine3D.a"
+  "libEngine3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Engine3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
