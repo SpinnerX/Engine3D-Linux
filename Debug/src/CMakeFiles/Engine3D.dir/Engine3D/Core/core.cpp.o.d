2a3059080339015a728e9bc44a6d63165ddd15b6
= <=>
2024-03-11 20:03:06 -0700

    Deleted ./Debug that is a build directory

--- Debug/src/CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/Engine3D.dir/Engine3D/Core/core.cpp.o: \
-  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/share/asan_ignorelist.txt \
-  /Users/aaronher/Desktop/Engine3D-Linux/src/Engine3D/Core/core.cpp
--- Debug/src/CMakeFiles/Engine3D.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Engine3D.
-# This may be replaced when dependencies are built.
